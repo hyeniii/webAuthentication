ca0e3670bfd59b54a77f9c1854d8ef43c7161b55
Nicole Hye Won Hwang <hyewonee96@gmail.com>
2022-12-19 11:24:13 -0600

    implemented OAuth from google in my website

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose"); // don't need to require passport-local
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 // const bcrypt = require("bcrypt");
 // const saltRounds = 10;
 // const md5 = require("md5");
@@ -33,25 +35,55 @@ mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy()); // authenticate user using username and password
 
-passport.serializeUser(User.serializeUser()); // create cookie and add user identification info
-passport.deserializeUser(User.deserializeUser()); // crumble cookie to find information inside cookie (authenticate user in server)
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+}); // create cookie and add user identification info
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        done(err,user);
+    });
+}); // crumble cookie to find information inside cookie (authenticate user in server)
 
-
+// add OAuth for google
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // googleId from userSchema
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 //////////////////////// API ////////////////////////////////
 app.get("/", function(req,res){
     res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }));
+
+  app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/secrets");
+  });
 
 app.get("/login", function(req,res){
     res.render("login");
